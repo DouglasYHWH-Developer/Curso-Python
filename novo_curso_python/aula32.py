a3a6fec1fa9b1e83a1f6dcae0daa8c0cb7086096
Douglas <dougurasu104@hotmail.com>
2024-12-18 12:16:24 -0300

    adicao da aula32

--- novo_curso_python/aula32.py
+++ novo_curso_python/aula32.py
@@ -4,3 +4,18 @@ None = Não valor
 is e is not = é ou não é (tipo, valor, identidade)
 id = Identidade
 """
+condicao = True
+passou_no_if = None
+
+if condicao:
+    passou_no_if = True
+    print('Faça algo')
+else:
+    print('Não faça algo')
+
+
+if passou_no_if is None:
+    print('Não passou no if')
+else:
+#if   passou_no_if is not None: 
+    print('Passou no if')
