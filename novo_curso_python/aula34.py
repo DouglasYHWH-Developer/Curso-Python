d76fb39e328f8429b5dfb2dd6332b2c1dc2a6b56
Douglas <dougurasu104@hotmail.com>
2024-12-26 09:59:26 -0300

    adicao das aulas 33doc e 34

---
+++ novo_curso_python/aula34.py
@@ -0,0 +1,10 @@
+"""
+Repetições
+while(enquanto)
+Executa uma ação enquantio uma condição for verdadeira
+Loop infito -> Qaundo um códifo não tem fim
+"""
+condicao = True
+
+while condicao:
+    nome = input('Qual o seu nome? ')
