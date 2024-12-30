ed82aaa375ffb5fa9d0bd687fecfe1e516008914
Douglas <dougurasu104@hotmail.com>
2024-12-30 11:10:23 -0300

    adição aula 42

---
+++ novo_curso_python/aula42.py
@@ -0,0 +1,29 @@
+frase = 'aaaaaaaaoooooo '
+
+i = 0
+qtd_apareceu_mais_vezes = 0
+letra_apareceu_mais_vezes = ''
+
+while i < len(frase):
+    letra_atual = frase[i]
+
+    if letra_atual == ' ':
+        i += 1
+        continue
+
+    qtd_apareceu_mais_vezes_atual =  frase.count(letra_atual)
+
+    if qtd_apareceu_mais_vezes < qtd_apareceu_mais_vezes_atual:
+        qtd_apareceu_mais_vezes = qtd_apareceu_mais_vezes_atual
+        letra_apareceu_mais_vezes = letra_atual
+
+    i += 1
+
+print(
+    'A letra que apareceu mais vezes foi '
+    f'"{letra_apareceu_mais_vezes}", que apareceu '
+    f'{qtd_apareceu_mais_vezes} x')
+
+"""
+Em resumo, para contar as ocorrências de um único item em uma lista, use o método count .
+"""
