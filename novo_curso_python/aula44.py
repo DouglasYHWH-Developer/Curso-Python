d0eb41f1d514a0e4359b6264ce9554a5bda3d680
Douglas <dougurasu104@hotmail.com>
2024-12-30 12:00:40 -0300

    adição da aula44 - for range

---
+++ novo_curso_python/aula44.py
@@ -0,0 +1,10 @@
+"""
+For + Range
+range -> range(start, stop, step)
+"""
+#numeros = range(10) stop foid e 0 10
+#numeros = range(5, 10) #start e stop foi de 5 a 10
+numeros = range(5, 10, 2)# start, stop e step foi de 5 a 10 pulando de 2 em 2
+
+for numero in numeros:
+    print (numero)
