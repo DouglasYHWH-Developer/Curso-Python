4eadfb9609da87f44605072444e13c6a4f06abec
Douglas <dougurasu104@hotmail.com>
2025-01-21 13:07:03 -0300

    atualização aula 50

--- novo_curso_python/aula50exercicio.py
+++ novo_curso_python/aula50exercicio.py
@@ -2,8 +2,16 @@
 for in com listas
 """
 lista = ['Maria', 'Helena', 'Luiz', "Douglas"]
+lista.append('João')
 
 i = 0
 for nome in lista:
     print(i, nome)
     i += 1
+
+# Solução do professor
+
+indices= range(len(lista))
+
+for indice in indices:
+    print(indice, lista[indice])
