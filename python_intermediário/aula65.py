77ae09998a3216a02d7c9aad46be8005e218491a
Douglas <dougurasu104@hotmail.com>
2025-02-17 12:34:59 -0300

    adição aula65

---
+++ python_intermediário/aula65.py
@@ -0,0 +1,8 @@
+"""
+    Introdução às funções (deg) em Python
+    Funções são trechos de código usados para replicar
+determinada ação ao longo do seu código.
+    Elas podem receber valores para parâmetros (argumentos)
+e retornar um valor específico.
+    Por padrão, funções em Python retornam none(nada)
+"""
