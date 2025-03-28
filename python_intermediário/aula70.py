6fa30764d7b1bbb8b8c979a1842a01b283e35c4f
Douglas <dougurasu104@hotmail.com>
2025-03-28 14:05:43 -0300

    adição aula71

---
+++ python_intermediário/aula70.py
@@ -0,0 +1,12 @@
+"""
+Retorno de valores das funções (return)
+"""
+
+def soma(x, y):
+    if x > 10:
+        return 10
+    return x + y
+
+soma1 = soma(1, 5)
+soma2 = soma(6, 5)
+print (soma1 + soma2)
