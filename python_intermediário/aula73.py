a8766b6286bf10867b7ebbcf86976b4616bf87a4
Douglas <dougurasu104@hotmail.com>
2025-04-02 14:40:37 -0300

    adi~ção aula73

---
+++ python_intermediário/aula73.py
@@ -0,0 +1,22 @@
+"""
+Higher Order Function
+Funções de primeira classe
+"""
+
+
+def saudacao(msg, nome):
+    return f'{msg}, {nome}!'
+
+
+def executa(funcao, *args):
+    return funcao(*args)
+
+
+print(
+    executa(saudacao,'Bom dia', 'Douglas')
+)
+print(
+    executa(saudacao,'Bom dia', 'Toguro')
+)
+
+
